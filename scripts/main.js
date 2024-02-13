23ea8c26c3a07ae145ab7532e88ecd84c9130fdf
brycenjwill <wil21062@byui.edu>
2024-02-13 00:04:32 -0700

    Basic layout of header and footers for all mobile pages

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function hamburger() {
+    var x = document.getElementById("navdrop");
+
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+;
+    }
+  } 
